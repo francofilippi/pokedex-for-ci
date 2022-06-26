4e4d95fe77a5e1137a5872f99c6cd81d01139c8f
francofilippi <francodanielfilippi@gmail.com>
2022-06-26 20:15:28 -0300

    add our first workflow of github actions

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -1,8 +1,8 @@
 /* global cy */
 
-describe('Pokedex', function() {
+describe('Pokedex', function () {
   it('frontpage can be opened', function () {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3000')
     cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
